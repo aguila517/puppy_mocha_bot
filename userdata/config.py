92ceb29249ef3ff2a3e22061ecf27cc6c8a83251
aguila517 <myun@apple.com>
2025-04-06 20:32:39 -0700

    Reset the original tee time threshold to 1pm

--- userdata/config.py
+++ userdata/config.py
@@ -1,6 +1,6 @@
 # tee time thresholds for bot to check
 hold_tee_time = {
-                    'latest_tee_time': '19:15', # Must be 24hr format
+                    'latest_tee_time': '13:15', # Must be 24hr format
                     'days': ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'], # 'Mon','Tue','Wed','Thu','Fri','Sat','Sun'
                     'number_of_players': 4, # Minimum number of available players in the tee time
                 }
